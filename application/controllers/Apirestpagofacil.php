353f6f91dd743e5c6204bf3ece5a27f0a871a27c
versace123mx <versace123mx@gmail.com>
2019-07-04 16:14:16 -0500

    primer commit pago facil

---
+++ application/controllers/Apirestpagofacil.php
@@ -0,0 +1,87 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Apirestpagofacil extends \Restserver\Libraries\REST_Controller {
+
+	public function __construct() {
+        parent::__construct();
+        $this->load->model('Escuela_model');
+    }
+
+	public function test_get()
+    {
+    	$array=array('Hola','Mundo','Codeigniter');
+    	$this->response($array);
+	}
+
+    public function user_get()
+    {
+    	$id = $this->input->get('idusuario');
+    	
+    	$regex="/^[[:digit:]]+$/";
+      	if(!preg_match($regex,$id)){
+        	return "El id es un digito";
+    	} else {
+        	$r  = $this->Escuela_model->read($id);
+        	$this->response($r);
+        }
+    }
+
+	/*Metodo para insertar un registro*/
+    public function user_post()
+    {
+
+    	$data = array(
+    	'idmateria' => $this->input->post('idmateria'),
+    	'idusuario' => $this->input->post('idusuario'),
+        'calificacion' => $this->input->post('calificacion')
+         );
+    	if(!empty($data['idmateria']) && !empty($data['idusuario']) && !empty($data['calificacion'])){
+    		$regex="/^[[:digit:]]+$/";
+      		if(preg_match($regex,$data['idmateria']) && preg_match($regex,$data['idusuario']) ){
+				$r = $this->Escuela_model->insert($data);
+        		$this->response($r);
+    		}else{
+    			return "Parametros invalidos,intentalo nuevamente";
+    		}
+    	}else{
+			return "Parametros invalidos,intentalo nuevamente";
+    	}
+         
+    }
+
+    /*Metodo para actualizar un registro*/
+    public function user_put()
+    {
+        $data = array(
+       	'idcalificacion' => $this->input->get('idcalificacion'),
+        'calificacion' => $this->input->get('calificacion')
+        );
+        
+        if(!empty($data['idcalificacion']) && !empty($data['calificacion'])){
+    		$regex="/^[[:digit:]]+$/";
+    		if(preg_match($regex,$data['idcalificacion'])){
+				$r = $this->Escuela_model->update($data);
+        		$this->response($r);
+    		}else{
+				return "Parametros invalidos,intentalo nuevamente";
+    		}	
+    	}
+	}
+
+
+    /*Metodo para eliminar un registro*/
+    public function user_delete()
+    {
+        $id = $this->input->get('idcalificacion');
+        if(!empty($data['idcalificacion'])){
+    		$regex="/^[[:digit:]]+$/";
+    		if(preg_match($regex,$data['idcalificacion'])){
+        		$r = $this->Escuela_model->delete($id);
+        		$this->response($r); 
+    		}
+		}
+    }
+}
---
+++ application/models/Escuela_model.php
@@ -0,0 +1,94 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Escuela_model extends CI_Model
+{
+  
+
+
+  public function __construct(){
+    $this->load->database();
+  }
+
+  public function read($id){
+    $iduser = $id;
+    if(empty($iduser)){
+        $iduser=0;
+    }
+   
+    $cadena='%d-%c-%Y';
+       $query = $this->db->query('
+        select ta.id_t_usuarios,ta.nombre,ta.ap_paterno,tm.nombre as materia,tc.calificacion,
+        date_format(tc.fecha_registro,"'.$cadena.'") as fecha_registros
+        from t_alumnos as ta 
+        inner join t_calificaciones as tc on ta.id_t_usuarios=tc.id_t_usuarios
+        inner join t_materias as tm on tc.id_t_materias= tm.id_t_materias
+        where ta.id_t_usuarios = "'.$iduser.'" and tm.activo=1 and ta.activo=1');
+       
+        $cadSQL = $query->result_array();
+
+        $sumCalificacion=0;
+        $numElementos = count($cadSQL);
+        if($numElementos > 0){
+          foreach ($cadSQL as $key) {
+            $sumCalificacion += $key['calificacion'];
+          }
+          $promedio=$sumCalificacion/$numElementos;
+          $cadSQL['promedio']=$promedio;
+        }
+
+       
+       if(!empty($cadSQL)){
+        return $cadSQL;
+       }else{
+        return "no hay datos a mostrar, verifica el id";
+       }
+
+   }
+
+   public function insert($data){
+       $this->id_t_materias  = $data['idmateria'];
+       $this->id_t_usuarios = $data['idusuario'];
+       $this->calificacion  = $data['calificacion'];
+       $this->fecha_registro = date("Y-m-d");
+       if($this->db->insert('t_calificaciones',$this))
+       {    
+           $arreglo = array("success" => "ok","msg" => "calificacion registrada");
+           return $arreglo;
+       }
+         else
+       {
+           return "Error has occured";
+       }
+   }
+
+   public function update($data){
+       $id  = $data['idcalificacion'];
+       $this->calificacion  = $data['calificacion'];
+       $result = $this->db->update('t_calificaciones',$this,array('id_t_calificaciones' => $id));
+       if(!empty($result))
+       {
+        $arreglo = array("success" => "ok","msg" => "calificacion actualizada");
+           return $arreglo;
+       }
+       else
+       {
+           return "Error has occurred";
+       }
+   }
+
+   public function delete($id){
+       $idcalificacion  = $id;
+       $this->activo=0;
+       $result = $this->db->query('delete from t_calificaciones where id_t_calificaciones = "'.$id.'"');
+       if($result)
+       {
+            $arreglo = array("success" => "ok","msg" => "calificacion eliminada");
+           return $arreglo;
+       }
+       else
+       {
+           return "Error has occurred";
+       }
+   }
+}
